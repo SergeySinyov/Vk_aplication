7e4229b8ef8472bc995d98f6043154799f00df1e
Sergey Sinyov <sinev_130910@mai.ru>
2022-03-23 14:44:57 +0600

    Домашняя работа выполненна

---
+++ Vk_aplication/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Vk_aplication
+//
+//  Created by Сергей Синёв on 23.03.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

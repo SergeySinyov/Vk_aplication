19a5d756fbbfbed47613441b019c27fad21b3a71
Sergey Sinyov <sinev_130910@mai.ru>
2022-06-11 15:41:14 +0600

    Домашняя работа №1 выполненна

---
+++ Vk_aplication/Session.swift
@@ -0,0 +1,17 @@
+//
+//  Session.swift
+//  Vk_aplication
+//
+//  Created by Сергей Синёв on 11.06.2022.
+//
+
+import UIKit
+
+class Session {
+
+    let session = Session()
+
+    var token: String = ""
+    var userId: Int = 0
+}
+

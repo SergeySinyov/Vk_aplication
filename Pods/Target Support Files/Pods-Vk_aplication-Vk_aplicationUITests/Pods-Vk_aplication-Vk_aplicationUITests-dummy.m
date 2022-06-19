bf411dbd774cdb7f576179007b03260e0c4419ea
Sergey Sinyov <sinev_130910@mai.ru>
2022-06-19 09:25:34 +0600

    доработка

---
+++ Pods/Target Support Files/Pods-Vk_aplication-Vk_aplicationUITests/Pods-Vk_aplication-Vk_aplicationUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Vk_aplication_Vk_aplicationUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Vk_aplication_Vk_aplicationUITests
+@end
